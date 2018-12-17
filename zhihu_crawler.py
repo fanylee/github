04a95dcbf5a16076bb71af9b057e41a6202b1d80
fanny <fangqing_lee@163.com>
2018-12-17 13:48:26 +0800

    crawler of  zhihu information

---
+++ zhihu_crawler.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import time
+
+def start_chrome():
+    driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+    driver.start_client()
+    return driver
+
+def find_strangers():
+    #btn
+    btn_sel = 'div.ContentItem-extra > button.Button--blue'
+    elems = driver.find_elements_by_css_selector(btn_sel)
+    return elems
+
+def add_fren():
+    pass
+
+while True:
+    url = ''
+    follow_url = ''
+    driver = start_chrome()
+    driver.get(url)
+    if not driver.get_cookie():
+        push()
+    time.sleep(20)#waitin login
+
+driver.get(follow_url)
+time.sleep(6)
+strangers = find_strangers()
+for s in strangers:
+    s.click()
+    time.sleep(3)
+
+
+print('Done')
+time.sleep(3000)
+
+
+
