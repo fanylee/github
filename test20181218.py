2d006bf6adfbb795c3d510962086a13341df3635
fanny <fangqing_lee@163.com>
2018-12-18 21:25:12 +0800

    learn to create a simple tool to take a screenshot

--- test20181218.py
+++ test20181218.py
@@ -2,10 +2,12 @@ from tkinter import *
 import os
 
 app = Tk()
-label = Label(text='All hiden files', font=('Hack', 25, 'bold'))
-label.pack()
-listbox = Listbox(bg='#f2f2f2', fg='blue')
+label = Label(text='All hiden files', font=('Hack', 25, 'bold')).pack()
+btn = Button(text='click me')
+listbox = Listbox(bg='#f12f12f12', fg='purple')
 listbox.pack(fill=BOTH, expand=True)
+btn.pack()
+
 path = r'D:\BigData\doc\Doc_HDP V2.6.0'
 files = os.listdir(path)
 for f in files:
