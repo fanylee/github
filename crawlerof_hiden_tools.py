dfb1356d1f81a6f38cb42779ea3a8670b85812f1
fanny <fangqing_lee@163.com>
2018-12-19 12:52:03 +0800

    add my favourite color aquamarine

--- crawlerof_hiden_tools.py
+++ crawlerof_hiden_tools.py
@@ -2,9 +2,9 @@ from tkinter import *
 import os
 
 app = Tk()
-label = Label(text='All hiden files', font=('Hack', 25, 'bold'))
+label = Label(text='List All hiden files', font=('Hack', 25, 'bold'))
 label.pack()
-listbox = Listbox(bg='#f2f2f2', fg='blue')
+listbox = Listbox(bg='#f2f2f2', fg='aquamarine')
 listbox.pack(fill=BOTH, expand=True)
 path = r'D:\BigData\doc\Doc_HDP V2.6.0'
 files = os.listdir(path)
