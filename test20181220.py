3988605cad16483b09bd9417064d1fc83373c265
fanny <fangqing_lee@163.com>
2018-12-20 21:45:22 +0800

    add interesting tricks

--- test20181220.py
+++ test20181220.py
@@ -3,9 +3,17 @@ import time
 from tkinter import *
 
 
+# while True:
+#    'en0'
+#    s1 = psutil.net_io_counters(pernic=True)
+#    time.sleep(1)
+#    s2 = psutil.net_io_counters(pernic=True)
+#    result = s2.bytes_recv - s1.bytes_recv
+#    print(result / 1024)
+
 def make_app():
     app = Tk()
-    app.geometry('300x300')
+    app.geometry('400x400')
     app.config(bg='#303030')
     Label(text='Speed Monitor',
           font=('Arial', 25, 'bold'),
@@ -22,20 +30,21 @@ def make_app():
 
 
 def speed_test():
-    s1 = psutil.net_io_counters(pernic=True)['lan']
+    # 'en0'
+    s1 = psutil.net_io_counters(pernic=True)['Wi-Fi']
     time.sleep(1)
-    s2 = psutil.net_io_counters(pernic=True)['lan']
+    s2 = psutil.net_io_counters(pernic=True)['Wi-Fi']
     result = s2.bytes_recv - s1.bytes_recv
     return str(result / 1024) + 'kb/s'
 
 
 def ui_update(do):
     data = do()
-    lb2 = app.children['lb2']
-    lb2.config(text=data)
+    lbe = app.children['lb2']
+    lbe.config(text=data)
     app.after(1000, lambda: ui_update(do))
 
 
 app = make_app()
-app.after(1000, lambda: ui_update(speed_test()))
+app.after(1000, lambda: ui_update(speed_test))
 app.mainloop()
