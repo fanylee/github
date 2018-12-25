5f13a6375eab01a20d89cd1db4f4873e9c00b255
fanny <fangqing_lee@163.com>
2018-12-25 21:27:51 +0800

    modify

--- test20181225.py
+++ test20181225.py
@@ -1,34 +1,27 @@
-import time
+# core --> core&ui --> core&ui&user --> Boundry.Edge.Corner
 from tkinter import *
-from multiprocessing import Process
+import time
 import threading
+from multiprocessing import Process
 
 info = {
-    'total_time': 60
+    'total_time': 0
 }
-
-flag = [False]
 
 
 def make_app():
     _font = ['Arial', 25, 'bold']
     app = Tk()
-    Label(name='lb', text='sand clock', fg='yellow', bg='blue', font=_font).pack()
+    Label(name='lb', text=0, font=_font).pack()
     Button(name='btn', text='start', command=time_counts).pack()
-    Button(name='bts', text='reset', command=time_reset).pack()
     Entry(name='ipt').pack()
-    Button(name='btp', text='pause', command=time_pause).pack()
-    app.geometry('300x300')
+    app.geometry('200x200')
     return app
 
 
 def time_counts():
-    flag[0] = False
-
     def _counts():
         while info['total_time']:
-            if flag[0]:
-                break
             info['total_time'] -= 1
             print(info['total_time'])
             time.sleep(1)
@@ -37,26 +30,9 @@ def time_counts():
     t.start()
 
 
-def time_reset():
-    flag[0] = True
-    info['total_time'] = 60
-
-
-def time_pause():
-    flag[0] = True
-
-
 def ui_watcher():
     def _update_button():
-        btn = app.children['btn']
-        btp = app.children['btp']
-        timer = [t for t in threading.enumerate() if t.name == 'timer']
-        if timer:
-            btn['state'] = 'disabled'
-            btp['state'] = 'normal'
-        else:
-            btn['state'] = 'normal'
-            btp['state'] = 'disable'
+        pass
 
     def _get_time():
 
@@ -66,19 +42,16 @@ def ui_watcher():
             info['total_time'] = int(ipt.get())
 
     def _update_time():
-
         lb = app.children['lb']
         lb['text'] = info['total_time']
 
     def _main():
         while True:
-            print("tik toc")
+            print('tic toc')
             print(threading.enumerate())
-            _update_button()
             _get_time()
+            _update_button()
             _update_time()
-
-            time.sleep(0.5)
 
     t = threading.Thread(target=_main, name='watcher')
     t.start()
@@ -86,5 +59,6 @@ def ui_watcher():
 
 if __name__ == '__main__':
     app = make_app()
-    app.after(0, ui_watcher)
+    app.after(0, time_counts)
+    app.after(1, ui_watcher)
     app.mainloop()
